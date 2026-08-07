7f18d8e0d3f297068dbeb7b7571dba109179474a
rcsanju6887 <rashedasanju@gmail.com>
2026-08-07 10:49:19 +0600

    Update AST semantic.h

--- Backend/java_compiler/src/semantic/semantic.h
+++ Backend/java_compiler/src/semantic/semantic.h
@@ -1,10 +1,15 @@
 #ifndef SEMANTIC_H
 #define SEMANTIC_H
 
+#include <vector>
+#include <string>
 #include "../ast/ast.h"
 
-void semanticCheck(ASTNode *root);
+using namespace std;
 
+extern vector<string> semanticLogs;
+
+void semanticCheck(ASTNode *root);
 void traverseAST(ASTNode *node);
 void processAssignment(ASTNode *node);
 string getExpressionType(ASTNode *node);
